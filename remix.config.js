ce651b56ef348efebc256b23fb1f58dfdbf7bdbb
Jasper Anelechukwu <jasperanels@gmail.com>
2022-05-31 21:38:07 +0100

    Added netlify configs

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,8 @@
  */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  serverBuildTarget: "netlify",
+  server: "./server.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
