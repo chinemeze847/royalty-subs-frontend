82956287b86e9b069635ace35a459820ab4cee16
Jasper Anelechukwu <jasperanels@gmail.com>
2022-07-16 00:30:05 +0100

    Added error views

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,17 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from "@remix-run/react";
 import styles from "./styles/tailwind.css";
 import toastStyle from 'react-toastify/dist/ReactToastify.css';
+import { IoWarning } from "react-icons/io5";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -21,7 +24,7 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: toastStyle },
 ];
 
-export default function App() {
+function Document({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
       <head>
@@ -29,11 +32,62 @@ export default function App() {
         <Links />
       </head>
       <body className="font-sans">
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+function ErrorBox({ message }: { message: string; }) {
+  return (
+   <>
+    <h1 className="text-color-primary font-bold text-3xl">
+      <Link to="/">Royaltysubs</Link>
+    </h1>
+
+    <div className="text-center shadow shadow-color-error rounded-lg p-dimen-sm my-dimen-lg">
+      <IoWarning className="mx-auto text-7xl text-color-error" />
+      <div className="font-bold my-dimen-sm text-2xl">{message}</div>
+    </div>
+   </>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document>
+      <div className="container my-dimen-xl">
+        <ErrorBox message={error.message} />
+
+        {
+          process.env.NODE_ENV === 'development' && (
+            <pre className="bg-pink-300 overflow-auto p-dimen-lg rounded-lg my-dimen-lg">{error.stack}</pre>
+          )
+        }
+      </div>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <Document>
+      <div className="container my-dimen-xl">
+        <ErrorBox message={`${caught.status} ${caught.statusText}`} />
+      </div>
+    </Document>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
   );
 }
